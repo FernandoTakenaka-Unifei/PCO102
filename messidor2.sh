7ca7795fd87f4c1ebb03030f8d964d28e6c87325
FernandoTakenaka-Unifei <86011841+FernandoTakenaka-Unifei@users.noreply.github.com>
2021-06-19 10:17:55 -0300

    Removido download do Messidor2
    Arquivos incluidos no projeto

--- messidor2.sh
+++ messidor2.sh
@@ -3,10 +3,10 @@
 
 # Assumes that the data set resides in ./data/messidor.
 
-messidor_download_url="http://webeye.ophth.uiowa.edu/abramoff/messidor-2.zip"
+#messidor_download_url="http://webeye.ophth.uiowa.edu/abramoff/messidor-2.zip"
 messidor_dir="./content/PCO102/data/messidor2"
-messidor_path="$messidor_dir/messidor-2.zip"
-grades_path="./content/PCO102/vendor/messidor/abramoff-messidor-2-refstandard-jul16.csv"
+#messidor_path="$messidor_dir/messidor-2.zip"
+grades_path="./content/PCO102/data/abramoff-messidor-2-refstandard-jul16.csv"
 default_output_dir="$messidor_dir/bin2"
 
 check_parameters()
@@ -28,47 +28,6 @@ check_parameters()
 
 strip_params=$(echo "$@" | sed "s/--\([a-z]\+\)\(=\(.\+\)\)\?/\1/g")
 check_parameters "$#" "$strip_params" "output"
-
-# Get output directory from parameters.
-output_dir=$(echo "$@" | sed "s/.*--output=\([^ ]\+\).*/\1/g")
-
-# Check if output directory is valid.
-if ! [[ "$output_dir" =~ ^[^-]+$ ]]; then
-  output_dir=$default_output_dir
-fi
-
-if ls "$output_dir" >/dev/null 2>&1; then
-  echo "Dataset is already located in $output_dir."
-  echo "Specify another output directory with the --output flag."
-  exit 1
-fi
-
-messidor_size=$(ls -s $messidor_path 2>/dev/null | cut -d " " -f1)
-
-if [ !ls "$messidor_path" >/dev/null 2>&1 ] || \
-    [[ $messidor_size -ne 3398280 ]]; then
-  echo "Downloading messidor-2.zip. This may take a while..."
-  curl -L0 "$messidor_download_url" --output "$messidor_path"
-fi
-
-count_files=$(ls $messidor_dir/Messidor-2 2>/dev/null | wc -l)
-
-if [ !ls "$messidor_dir/Messidor-2" >/dev/null 2>&1 ] || \
-    [[ $count_files -ne 1757 ]]; then
-  # Check if unzip has been installed.
-  dpkg -l | grep unzip
-  if [ $? -gt 0 ]; then
-    echo "Please install unzip: apt-get/yum install unzip" >&2
-    exit 1
-  fi
-
-  if [[ $count_files -ne 1757 ]]; then
-    echo "Messidor-2 wasn't unpacked properly before"
-  fi
-
-  echo "Unpacking messidor-2.zip"
-  unzip "$messidor_path" -d "$messidor_dir" || exit 1
-fi
 
 # Copying labels file from vendor to data directory.
 cp "$grades_path" "$messidor_dir/labels.csv"
