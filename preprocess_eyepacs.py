de6acbac0dcb29a9d483e72ea855f84178f6353e
FernandoTakenaka-Unifei <86011841+FernandoTakenaka-Unifei@users.noreply.github.com>
2021-06-22 11:30:23 -0300

    Update preprocess_eyepacs.py

--- preprocess_eyepacs.py
+++ preprocess_eyepacs.py
@@ -73,7 +73,7 @@ for labels in [test_labels]:
             basename, grade = row[:2]
 
             #im_path = glob(join(data_dir, "{}*".format(basename)))[0]
-            print(glob(join(train, "{}*".format(basename)))[0])
+            print(glob(join(train, "{}*".format(basename))))
             im_path = glob(join(train, "{}*".format(basename)))[0]
 
             # Find contour of eye fundus in image, and scale
